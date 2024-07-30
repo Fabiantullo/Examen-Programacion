f72bd7a0c01155cc323a1118cf7cc4a41afd18c4
Fabiantullo <fabiantullorancati@gmail.com>
2024-07-30 13:21:03 -0300

    Initial commit

---
+++ preparacion.py
@@ -0,0 +1,92 @@
+"De 5 mascotas que ingresan a una veterinaria se deben tomar y validar los siguientes datos."
+#Nombre
+#Tipo (gato ,perro o exotico)
+#Peso ( entre 10 y 80)
+#Sexo( F o M )
+#Edad(mayor a 0)
+#Pedir datos por prompt y mostrar por print, se debe informar:
+#Informe A- Cuál fue el tipo mas ingresado (gato ,perro o exotico)
+#Informe B- El porcentaje de mascotas femeninas y el de las masculinas.
+#Informe C -El tipo de la mascota más pesada
+#Informe D- El nombre del gato más joven
+#Informe E- El promedio de peso de todas las mascotas
+contador = 0
+contador_gatos = 0
+contador_perros = 0
+contador_exoticos = 0
+contador_femeninas = 0
+contador_masculinas = 0
+peso_mascota_pesada = 0
+edad_gato_joven = 0
+acumulador_peso = 0
+
+while contador < 5:
+    nombre_ingresado = input("Ingrese el nombre de la mascota: ")
+
+    tipo_ingresado = input("Ingrese el tipo de la mascota (gato, perro o exotico):  ")
+    while tipo_ingresado != "gato" and tipo_ingresado != "perro" and tipo_ingresado != "exotico":
+        tipo_ingresado = input("ReIngrese el tipo de la mascota (gato, perro o exotico):  ")
+
+    peso_ingresado = int(input("Ingrese el peso de su mascota (entre 10 y 80):  "))
+    while peso_ingresado < 10 or peso_ingresado > 80:
+        peso_ingresado = int(input("Reingrese el peso de su mascota (entre 10 y 80):  "))
+
+    sexo_ingresado = input("Ingrese el sexo de la mascota (F o M):  ")
+    while sexo_ingresado != "F" and sexo_ingresado != "M":
+        sexo_ingresado = input("Reingrese el sexo de la mascota (F o M):  ")
+
+    edad_ingresada = int(input("Ingrese la edad de la mascota (mayor a 0):  "))
+    while edad_ingresada < 0:
+        edad_ingresada = int(input("Reingrese la edad de la mascota (mayor a 0):  "))
+
+    match tipo_ingresado:
+        case "gato":
+            if contador_gatos == 0:
+                edad_gato_joven = edad_ingresada
+                nombre_gato_joven = nombre_ingresado
+            elif edad_ingresada < edad_gato_joven:
+                edad_gato_joven = edad_ingresada
+                nombre_gato_joven = nombre_ingresado
+            contador_gatos += 1
+        
+        case "perro":
+            contador_perros += 1
+        case _ :
+            contador_exoticos += 1
+    
+    match sexo_ingresado:
+        case "F":
+            contador_femeninas += 1
+        case _ :
+            contador_masculinas += 1
+
+    match peso_ingresado:
+        case peso if peso > peso_mascota_pesada:
+            peso_mascota_pesada = peso
+            tipo_mascota_pesada = tipo_ingresado
+
+    acumulador_peso += peso_ingresado
+    contador += 1
+
+#Informe B- El porcentaje de mascotas femeninas y el de las masculinas.
+porcentaje_femeninas = (contador_femeninas / contador) * 100
+porcentaje_masculinas = (contador_masculinas / contador) * 100
+#Informe E- El promedio de peso de todas las mascotas
+promedio_peso = acumulador_peso / contador
+
+#Informe A- Cuál fue el tipo mas ingresado (gato ,perro o exotico)
+if contador_gatos > contador_perros and contador_gatos > contador_exoticos:
+    print("El tipo mas ingresado fue gato")
+elif contador_perros > contador_exoticos:
+    print("El tipo mas ingresado fue perro")
+else:
+    print("El tipo mas ingresado fue exotico")
+#Informe B- El porcentaje de mascotas femeninas y el de las masculinas. 
+print(f"El porcentaje de mascotas femeninas es {porcentaje_femeninas}%")
+print(f"El porcentaje de mascotas masculinas es {porcentaje_masculinas}%")
+#Informe C -El tipo de la mascota más pesada
+print(f"El tipo de la mascota más pesada es {tipo_mascota_pesada}")
+#Informe D- El nombre del gato más joven
+print(f"El nombre del gato más joven es {nombre_gato_joven}")
+#Informe E- El promedio de peso de todas las mascotas
+print(f"El promedio de peso de todas las mascotas es {promedio_peso}")
---
+++ preparacion2.py
@@ -0,0 +1,100 @@
+"""
+
+Se realiza un estudio de mercado para determinar cuál será la nueva franquicia 
+que se insertará en el mercado argentino y en la cual invertirán.
+Las posibles franquicias son las siguientes:
+    The north face
+    Zara
+    Bershka
+
+Para ello, se realizará una encuesta mediante un sistema de voto electrónico, 
+con el propósito de conocer cuáles son los gustos de los encuestados.
+
+Datos a ingresar:
+    Nombre del encuestado
+    Edad (no menor a 18)
+    Género (Masculino - Femenino - Otro)
+    Voto (STARBUCKS, MCDONALDS, ZARA, KFC)
+    Situación Laboral (Empleado - Desempleado)
+
+
+
+Consignas:
+    a-Nombre y situación laboral de la persona con mayor edad que voto por Zara.
+    b-Nombre y voto de la persona de sexo Otro de entre 60 y 70 años.
+    c-Cantidad de encuestados desempleados que votaron por Starbucks, cuya edad esté entre 30 y 45 años inclusive.
+    d-Quien de todos los sexos fue el que mas votó por Zara.
+    """
+
+seguir = "si"
+bandera_zara = 0
+edad_mayor_zara = 0
+edad_otro_genero = 0
+desempleado_starbucks = 0
+contador_zara_masculino = 0
+contador_zara_femenino = 0
+contador_zara_otro = 0
+
+while seguir == "si":
+    nombre_ingresado = input("Ingrese su nombre: ")
+
+    edad_ingresada = int(input("Ingrese su edad: "))
+    while edad_ingresada < 18:
+        edad_ingresada = int(input("Reingrese su edad: "))
+
+    genero_ingresado = input("Ingrese su genero (Masculino, Femenino o Otro):  ")
+    while genero_ingresado != "Masculino" and genero_ingresado != "Femenino" and genero_ingresado != "Otro":
+        genero_ingresado = input("Reingrese su genero:  ")
+    
+    voto_ingresado = input("Ingrese su voto (STARBUCKS, MCDONALDS, ZARA, KFC):  ")
+    while voto_ingresado != "STARBUCKS" and voto_ingresado != "MCDONALDS" and voto_ingresado != "ZARA" and voto_ingresado != "KFC":
+        voto_ingresado = input("Reingrese su voto:  ")
+    situacion_laboral_ingresada = input("Ingrese su situacion laboral (Empleado o Desempleado):  ")
+    while situacion_laboral_ingresada != "Empleado" and situacion_laboral_ingresada != "Desempleado":
+        situacion_laboral_ingresada = input("Reingrese su situacion laboral:  ")
+
+    match voto_ingresado:
+        case "ZARA":
+            if bandera_zara == 0:
+                nombre_mayor_edad_zara = nombre_ingresado
+                edad_mayor_zara = edad_ingresada    
+                situacion_laboral_mayor_zara = situacion_laboral_ingresada
+                bandera_zara = 1
+            elif edad_mayor_zara < edad_ingresada:
+                nombre_mayor_edad_zara = nombre_ingresado
+                edad_mayor_zara = edad_ingresada    
+                situacion_laboral_mayor_zara = situacion_laboral_ingresada
+            match genero_ingresado:
+                case "Masculino":
+                    contador_zara_masculino += 1
+                case "Femenino":
+                    contador_zara_femenino += 1
+                case _ :
+                    contador_zara_otro += 1
+                
+        case "STARBUCKS":
+            if edad_ingresada >= 30 and edad_ingresada <= 45:
+                desempleado_starbucks += 1
+
+    match genero_ingresado:
+        case "Otro":
+            if edad_ingresada > 60 and edad_ingresada < 70:
+                nombre_otro_genero = nombre_ingresado
+                voto_otro_genero = voto_ingresado
+                edad_otro_genero = edad_ingresada
+
+
+    # a-Nombre y situación laboral de la persona con mayor edad que voto por Zara.
+    # b-Nombre y voto de la persona de sexo Otro de entre 60 y 70 años.
+    # c-Cantidad de encuestados desempleados que votaron por Starbucks, cuya edad esté entre 30 y 45 años inclusive.
+    # d-Quien de todos los sexos fue el que mas votó por Zara.
+
+print(f"a-Nombre y situación laboral de la persona con mayor edad que voto por zara: {nombre_mayor_edad_zara} y {situacion_laboral_mayor_zara}.")
+print(f"b-Nombre y voto de la persona de sexo Otro de entre 60 y 70 años: {nombre_otro_genero} y su voto fue {voto_otro_genero}")
+print(f"c-Cantidad de encuestados desempleados que votaron por starbucks, cuya edad esta entre 30 y 45 años inclusive es: {desempleado_starbucks}")
+if contador_zara_masculino > contador_zara_femenino and contador_zara_masculino > contador_zara_otro:
+    print(f"d-El sexo que mas votó por zara es el masculino")
+elif contador_zara_femenino > contador_zara_otro:
+    print(f"d-El sexo que mas votó por zara es el femenino")
+else:
+    print(f"d-El sexo que mas votó por zara es el otro")
