3b8e70fef320594db1f04f8d4b736fee7ab8b5d0
Fabiantullo <fabiantullorancati@gmail.com>
2024-08-06 00:43:40 -0300

    commit

--- revision.py
+++ revision.py
@@ -1,115 +1,127 @@
-# Damian Sosa
-# Examen Final
+# Usuarios vendedores de MercadoLibre
+# Cargar 10 usuarios/vendedores de MercadoLibre con sus respectivas
+# publicaciones.
+# ● Los datos que se cargarán son:
+# ● Nombre de usuario
+# ● Edad (validar)
+# ● Cantidad de productos (validar-número entero positivo).
+# ● Número de publicaciones (validar-número entero positivo, hasta 1000).
+# ● Tipo ("producto", "servicio", "subasta")
+# ● Cuenta activa ("si", "no")
 
-# UTN Inversiones, realiza un estudio de mercado para saber cuál será la nueva franquicia que se insertará en el mercado argentino y en la cual invertirán.
-# Las posibles franquicias son las siguientes: Apple, Dunkin o Ikea.
-# Para ello, se realizará una encuesta mediante un sistema de voto electrónico, con el propósito de conocer cuáles son los gustos de los encuestados (no se sabe cuántos):
-# Se ingresa de cada encuestado:
-# ● nombre
-# ● edad (no menor a 18)
-# ● está empleado (si-no)
-# ● género (Masculino - Femenino - Otro)
-# ● voto (APPLE, DUNKIN, IKEA)
-# Se necesita saber:
-# 1. Cantidad de encuestados de género Masculino, cuya edad esté entre 18 y 25 o entre 36 y 49 o que votaron por IKEA.
-# 2. Nombre y género del encuestado de menor edad que votó por APPLE.
-# 3. Porcentaje de encuestados de género Femenino que votaron por IKEA con edad mayor a 25 años.
-# 4. Edad promedio de los encuestados que están o no empleados (de cada uno).
-# 5. Determinar cuál fue la franquicia más votada.
+# Tema B:
 
-seguir = "Si"
-encuestados_genero_masculino = 0
-primer_voto_apple = 0
-encuestados_género_femenino_votaron_ikea_edad_mayor_25 = 0
-acumulador_edad_empleados = 0
-acumulador_edad_desempleados = 0
-total_encuestados_empleados = 0
-total_encuestados_desempleados = 0
-total_votos_apple = 0
-total_votos_dunkin = 0
-total_votos_ikea = 0
-total_votantes = 0
+# 1. Cantidad de usuarios con la cuenta inactiva cuyo producto sea del tipo
+# “servicio”, cuya edad no supere los 55 años.
 
-while seguir == "Si":
-    
-    nombre_del_encuestado = input("Ingrese su nombre: ")
-    
-    edad_del_encuestado = int(input("Ingrese su edad (Mayor a 18): "))
-    while edad_del_encuestado < 18:
-        edad_del_encuestado = int(input("Reingrese su edad: "))
+# 2. Nombre y tipo de publicacion de mayor edad con menos de 500
+# publicaciones.
 
-    situacion_laboral = input("Ingrese su situacion laboral (Empleado - Desempleado): ")
-    while situacion_laboral != "Empleado" and situacion_laboral != "Desempleado":
-        situacion_laboral = input("Reingrese su sitaucion laboral: ")
-    
-    genero_del_encuestado = input("Ingrese su genero (Masculino - Femenino - Otro): ")
-    while genero_del_encuestado != "Masculino" and genero_del_encuestado != "Femenino" and genero_del_encuestado != "Otro":
-        genero_del_encuestado = input("Reingrese su genero: ")
-    
-    voto_ingresado = input("Ingrese su voto (APPLE, DUNKIN, IKEA): ")
-    while voto_ingresado != "APPLE" and voto_ingresado != "DUNKIN" and voto_ingresado != "IKEA":
-        voto_ingresado = input("Reingrese su voto: ")
+# 3. Porcentaje de cuentas activas.
 
-# 1. Cantidad de encuestados de género Masculino, cuya edad esté entre 18 y 25 o entre 36 y 49 o que votaron por IKEA.
+# 4. Mostrar el promedio de edad de los usuarios cuyas publicaciones fueron 
+# del tipo “servicio”.
 
-    if genero_del_encuestado == "Masculino":
-        if edad_del_encuestado > 18 and edad_del_encuestado < 25 or edad_del_encuestado > 36 and edad_del_encuestado < 49 or voto_ingresado == "IKEA":
-            encuestados_genero_masculino += 1
+# 5. Determinar el tipo con menos publicaciones, cuya cuenta se encuentre
+# “activa”.
+contador_cuentas = 0
+cont_ingresos = 0
+contador_activas = 0
+contador_productos_activo = 0
+contador_subasta_activo = 0
+contador_servicio_activo = 0
+contador_servicio = 0
+acumulador_edad = 0
+contador_usuarios_servicio_menos_55 = 0
+bandera_mayor_edad = 0
+mayor_edad = 0
 
-# 2. Nombre y género del encuestado de menor edad que votó por APPLE.
+while cont_ingresos < 10:
+    nombre = input("ingrese su nombre: ")
+    edad = int(input("ingrese su edad: "))
+    while edad < 18:
+        edad = int(input("REingrese su edad: "))
+    cant_productos = int(input("ingrese la cantidad de productos: "))
+    while cant_productos < 0:
+        cant_productos = int(input("REingrese la cantidad de productos: "))
+    num_publicaciones = int(input("ingrese un numero de publicaciones (no mayor a 1000): "))
+    while num_publicaciones < 0 or num_publicaciones > 1000:
+        num_publicaciones = int(input("ingrese un numero de publicaciones (no mayor a 1000): "))
+    tipo = input("ingresa el tipo: ")
+    while tipo != "producto" and tipo != "servicio" and tipo != "subasta":
+        tipo = input("ingresa el tipo: ")
+    cuenta_activa = input("ingrese cuanta activa (si/no): ")
+    while cuenta_activa != "si" and cuenta_activa != "no":
+        cuenta_activa = input("REIngrese cuanta activa (si/no): ")
 
-    if voto_ingresado == "APPLE":
-        if primer_voto_apple == 0:
-            encuestado_de_menor_edad_voto_apple = edad_del_encuestado
-            nombre_votante_apple = nombre_del_encuestado
-            genero_votante_apple = genero_del_encuestado
-            primer_voto_apple = 1
-        elif edad_del_encuestado < encuestado_de_menor_edad_voto_apple:
-            nombre_votante_apple = nombre_del_encuestado
-            genero_votante_apple = genero_del_encuestado
+# 1. Cantidad de usuarios con la cuenta inactiva cuyo producto sea del tipo
+# “servicio”, cuya edad no supere los 55 años.
 
-# 3. Porcentaje de encuestados de género Femenino que votaron por IKEA con edad mayor a 25 años.
+    contador_cuentas +=1
 
-    if genero_del_encuestado == "Femenino":
-        if voto_ingresado == "IKEA":
-            if edad_del_encuestado > 25:
-                encuestados_género_femenino_votaron_ikea_edad_mayor_25 += 1
-
-# 4. Edad promedio de los encuestados que están o no empleados (de cada uno).
-
-    if situacion_laboral == "Empleado":
-        acumulador_edad_empleados += edad_del_encuestado
-        total_encuestados_empleados += 1
+    if cuenta_activa:
+        contador_activas +=1
+    if tipo == "producto":
+        contador_productos_activo =+1
+    elif tipo =="subasta":
+        contador_subasta_activo +=1
     else:
-        acumulador_edad_desempleados += edad_del_encuestado
-        total_encuestados_desempleados += 1
+        contador_servicio_activo +=1
+    if tipo == "servicio":
+        contador_servicio +=1
+        acumulador_edad += edad
+    elif edad < 55:
+        contador_usuarios_servicio_menos_55 =+1
 
-# 5. Determinar cuál fue la franquicia más votada.
+    
 
-    if voto_ingresado == "APPLE":
-        total_votos_apple += 1
-    elif voto_ingresado == "DUNKIN":
-        total_votos_dunkin += 1
-    else:
-        total_votos_ikea += 1
+# 2. Nombre y tipo de publicacion de mayor edad con menos de 500
+# publicaciones.
+    if num_publicaciones < 500:
+        if bandera_mayor_edad == 0:
+            mayor_edad = edad
+            nombre_edad_mayor = nombre
+            tipo_mayor_edad = tipo
+            bandera_mayor_edad = 1
+        elif edad > mayor_edad:
+            mayor_edad = edad
+            nombre_edad_mayor = nombre
+            tipo_mayor_edad = tipo
 
-    total_votantes += 1
+# 3. Porcentaje de cuentas activas.
 
-    seguir = input ("Desea continuar el programa Si o No: ")
+porcentaje_activas = (contador_activas / contador_cuentas) * 100
+if contador_servicio > 0:
+    promedio_edad = acumulador_edad / contador_servicio
 
-porcentaje_genero_femenino_votaron_ikea_mayor_25 = encuestados_género_femenino_votaron_ikea_edad_mayor_25 * 100 / total_votantes
+# 4. Mostrar el promedio de edad de los usuarios cuyas publicaciones fueron 
+# del tipo “servicio”.
 
-edad_promedio_encuestados_empleados = acumulador_edad_empleados / total_encuestados_empleados
-edad_promedio_encuestados_desempleados = acumulador_edad_desempleados / total_encuestados_desempleados
+# 5. Determinar el tipo con menos publicaciones, cuya cuenta se encuentre
+# “activa”.
 
-print(f"La cantidad de encuestados de género Masculino, cuya edad esté entre 18 y 25 o entre 36 y 49 o que votaron por IKEA son {encuestados_genero_masculino}.")
-print(f"El encuestado de menor edad que votó por APPLE se llama {nombre_votante_apple} y su genero es {genero_votante_apple}.")
-print(f"El porcentaje de encuestados de género Femenino que votaron por IKEA con edad mayor a 25 años es {porcentaje_genero_femenino_votaron_ikea_mayor_25}.")
-print(f"La edad promedio de los encuestados que estan empleados es {edad_promedio_encuestados_empleados}, y la de los encuestados desempleados es {edad_promedio_encuestados_desempleados}.")
-
-if total_votos_apple > total_votos_dunkin and total_votos_apple > total_votos_ikea:
-    print(f"La franquicia mas votada es Apple con {total_votos_apple} votos.")
-elif total_votos_dunkin > total_votos_ikea:
-    print(f"La franquicia mas votada es Dunkin con {total_votos_dunkin} votos.")
+if contador_productos_activo < contador_subasta_activo and contador_subasta_activo < contador_servicio_activo:
+    menos_publicaciones = "subasta"
+elif contador_servicio_activo < contador_subasta_activo and contador_subasta_activo < contador_productos_activo:
+    menos_publicaciones = "servicio"
 else:
-    print(f"La franquicia mas votada es Ikea con {total_votos_ikea} votos.")
+    menos_publicaciones = "producto"
+
+if contador_usuarios_servicio_menos_55 >0:
+    print(f"1. Cantidad de usuarios con la cuenta inactiva cuyo producto sea del tipo “servicio”, cuya edad no supere los 55 años. {contador_usuarios_servicio_menos_55}")
+else:
+    print("NO se ingreso.")
+
+if bandera_mayor_edad > 0:
+    print(f"# 2. Nombre y tipo de publicacion de mayor edad con menos de 500 publicaciones.{nombre_edad_mayor} y {tipo_mayor_edad}")
+else:
+    print("no se ingreso lo pedido. ")
+
+print(f"Porcentaje de cuentas activas: {porcentaje_activas} '%'")
+if contador_servicio > 0:
+    print(f"Promedio Edad de usuarios en pub. de tipo 'servicio': {promedio_edad}")
+else: 
+    print("No hay publicaciones de tipo 'servicio'") 
+
+
+
